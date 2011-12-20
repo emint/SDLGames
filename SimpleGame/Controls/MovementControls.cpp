ba9a728ca32649251902b49935d74e8fd4cbab7a
emint <emint@Mayral.(none)>
2011-12-20 15:03:02 -0800

    Wrote new make file and started adding support for character movement

---
+++ SimpleGame/Controls/MovementControls.cpp
@@ -0,0 +1,17 @@
+/*
+ * MovementControls.cpp
+ *
+ *  Created on: Dec 20, 2011
+ *      Author: emint
+ */
+
+#include "MovementControls.h"
+
+MovementControls::MovementControls() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MovementControls::~MovementControls() {
+	// TODO Auto-generated destructor stub
+}
