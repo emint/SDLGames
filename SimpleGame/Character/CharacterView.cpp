1ddb36c68db98b1c9733e578b36974b90971fbc5
emint <emint@Mayral.(none)>
2011-12-22 17:26:00 -0800

    Adding character MVC and hooking up through tests.

---
+++ SimpleGame/Character/CharacterView.cpp
@@ -0,0 +1,17 @@
+/*
+ * CharacterView.cpp
+ *
+ *  Created on: Dec 22, 2011
+ *      Author: emint
+ */
+
+#include "CharacterView.h"
+
+CharacterView::CharacterView() {
+  // TODO Auto-generated constructor stub
+
+}
+
+CharacterView::~CharacterView() {
+  // TODO Auto-generated destructor stub
+}
